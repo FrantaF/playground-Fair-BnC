845321f2cd1f73d5385719a0b8cd67fc8626e4cd
shanqyeet <shanqyeet@gmail.com>
2018-01-03 21:33:08 +0800

    completed restful routes for user and listings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 
 class UsersController < Clearance::UsersController
-  def index
-    @listings = Listing.order(:updated_at).page params[:page]
-    @user = User.new
+  def home
+    @listings = Listing.order(updated_at: :desc).page params[:page]
     # @listpage = Listing.order(:update_at).page params[:page]
   end
 
@@ -12,9 +11,30 @@ class UsersController < Clearance::UsersController
     redirect_to '/'
   end
 
+  def show
+    @user = User.find(params[:id])
+    @user_listing = Listing.where(user_id: @user.id).page params[:page]
+    # @user_listing.order(:updated_at).page params[:page]
+  end
+
+  def index
+    @users = User.order(:updated_at).page params[:page]
+  end
+
+  def update
+    @update = User.find(params[:id])
+    @update.update(update_params)
+    redirect_to '/'
+  end
+
+
 
 private
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name, :password, :birthdate)
+  end
+
+  def update_params
+    params.require(:user).permit(:first_name,:last_name,:email,:birthdate,:country,:phone,:gender)
   end
 end
